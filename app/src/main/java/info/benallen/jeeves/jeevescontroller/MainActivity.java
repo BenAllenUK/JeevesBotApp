3276190fdf5db21296dfa0743444ca455c9d5939
Johannes Martinus van der Heijden <maartie0@eduroam-58-35.nomadic.bris.ac.uk>
2017-01-28 19:28:14 +0000

    beacon recognition added

--- app/src/main/java/info/benallen/jeeves/jeevescontroller/MainActivity.java
+++ app/src/main/java/info/benallen/jeeves/jeevescontroller/MainActivity.java
@@ -1,6 +1,7 @@
 package info.benallen.jeeves.jeevescontroller;
 
 import android.os.Handler;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,19 +11,114 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.koushikdutta.async.http.WebSocket;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.utils.UrlBeaconUrlCompressor;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BeaconConsumer, MonitorNotifier, RangeNotifier {
     private final String TAG = "Main";
     private final int interval = 1000;
+
+    private BeaconManager mBeaconManager;
+    private Map<String,Double> mIdToDistance = new HashMap<String,Double>();
 
 
     private Handler handler = new Handler();
     private Gson mGson = new Gson();
     private SocketHandler mSocketHandler = new SocketHandler();
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        // Detect the URL frame:
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_URL_LAYOUT));
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT));
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+        mBeaconManager.bind(this);
+    }
+
+    public void onBeaconServiceConnect() {
+        Region region = new Region("all-beacons-region", null, null, null);
+        try {
+            mBeaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        mBeaconManager.setMonitorNotifier(this);
+        mBeaconManager.setRangeNotifier(this);
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        for (Beacon beacon: beacons) {
+            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x10) {
+                // This is a Eddystone-URL frame
+                String url = UrlBeaconUrlCompressor.uncompress(beacon.getId1().toByteArray());
+                Log.d(TAG, "I see a beacon transmitting a url: " + url + " approximately " + beacon.getDistance() + " meters away.");
+                updateMap(beacon);
+            }
+        }
+        Log.d(TAG,getCurrentData());
+    }
+
+    private void updateMap(Beacon beacon){
+        if(mIdToDistance.containsKey(beacon.getId1().toString())){
+            mIdToDistance.remove(beacon.getId1().toString());
+        }
+        mIdToDistance.put(beacon.getId1().toString(),beacon.getDistance());
+    }
+
+    private String getCurrentData(){
+        StringBuilder stringBuilder = new StringBuilder();
+        NumberFormat formatter = new DecimalFormat("#0.0000");
+        for (String key : mIdToDistance.keySet()){
+            stringBuilder.append("ID:");
+            stringBuilder.append(key);
+            stringBuilder.append(", DIST:");
+            stringBuilder.append(formatter.format(mIdToDistance.get(key)));
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mBeaconManager.unbind(this);
+    }
+
+    @Override
+    public void didEnterRegion(Region region) {
+        Log.d(TAG,"1");
+    }
+
+    @Override
+    public void didExitRegion(Region region) {
+        Log.d(TAG,"2");
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+        Log.d(TAG,"3");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
